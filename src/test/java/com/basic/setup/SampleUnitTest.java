edc95e5b66535ea8ab4a8f4999186444cdb3f264
Nageswararao <nageswararao.99j@gmail.com>
2017-04-12 16:50:27 +0200

    Adding new Junit class

---
+++ src/test/java/com/basic/setup/SampleUnitTest.java
@@ -0,0 +1,14 @@
+package com.basic.setup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class SampleUnitTest {
+
+	@Test
+	public void test1(){
+		System.out.println("sample test");
+	}
+}
